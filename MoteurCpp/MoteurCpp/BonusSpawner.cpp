b6e4ba57b4e170b434a7b14c1ab862560daccd01
Mute <40270424+Ced0@users.noreply.github.com>
2023-03-17 14:59:19 +0100

    Fixing mickael's bug

--- MoteurCpp/MoteurCpp/BonusSpawner.cpp
+++ MoteurCpp/MoteurCpp/BonusSpawner.cpp
@@ -5,27 +5,27 @@
 
 void BonusSpawner::update(char input)
 {
-	int toSpawn = randInt(0, spawnMax);
+	//int toSpawn = randInt(0, spawnMax);
 
-	int cpt = 0;
-	for (int i = 0; i < objects.size() && cpt < toSpawn; i++)
-	{
-		if (objects[i]->active == false)
-		{
-			glm::ivec2 potatoes;
-			potatoes.x = 49;
+	//int cpt = 0;
+	//for (int i = 0; i < objects.size() && cpt < toSpawn; i++)
+	//{
+	//	if (objects[i]->active == false)
+	//	{
+	//		glm::ivec2 potatoes;
+	//		potatoes.x = 49;
 
-			do
-			{
-				potatoes.y = randInt(1, 19);
+	//		do
+	//		{
+	//			potatoes.y = randInt(1, 19);
 
-			} while (GameManager::getInstance()->getScreenValue(potatoes) == 'X');
+	//		} while (GameManager::getInstance()->getScreenValue(potatoes) == 'X');
 
 
-			((AsciiComponent*)objects[i]->components[0])->pos = potatoes;
+	//		((AsciiComponent*)objects[i]->components[0])->pos = potatoes;
 
-			objects[i]->active = true;
-			cpt++;
-		}
-	}
+	//		objects[i]->active = true;
+	//		cpt++;
+	//	}
+	//}
 }
